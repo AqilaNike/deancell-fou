8e360847b5debbbf30ff00418e6e110bde70a273
AqilaNike <aqilanikeindriani_2428240130@mhs.mdp.ac.id>
2026-06-20 01:13:14 +0700

    Update application-logo.blade.php

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,5 +1,5 @@
-<svg viewBox="0 0 650 120" xmlns="http://www.w3.org/2000/svg" {{ $attributes }}>
-    <text x="50%" y="55%" dominant-baseline="middle" text-anchor="middle" font-family="system-ui, -apple-system, sans-serif" font-size="90" font-weight="900" fill="currentColor" style="color: #14b8a6;">
+<svg viewBox="0 0 650 500" xmlns="http://www.w3.org/2000/svg" {{ $attributes }}>
+    <text x="50%" y="55%" dominant-baseline="middle" text-anchor="middle" font-family="system-ui, -apple-system, sans-serif" font-size="150" font-weight="900" fill="currentColor" style="color: #14b8a6;">
         Deanova <tspan style="color: #1e293b;" fill="currentColor">ERP</tspan>
     </text>
 </svg>
