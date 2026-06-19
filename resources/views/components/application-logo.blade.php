0b8d9d108a0be21c01fc0f33c457aea229a8aa56
AqilaNike <aqilanikeindriani_2428240130@mhs.mdp.ac.id>
2026-06-20 01:04:25 +0700

    style: increase logo font size and tighten viewBox

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,5 +1,5 @@
-<svg viewBox="0 0 500 100" xmlns="http://www.w3.org/2000/svg" {{ $attributes }}>
-    <text x="50%" y="55%" dominant-baseline="middle" text-anchor="middle" font-family="system-ui, -apple-system, sans-serif" font-size="55" font-weight="900" fill="currentColor" style="color: #14b8a6;">
+<svg viewBox="0 0 460 100" xmlns="http://www.w3.org/2000/svg" {{ $attributes }}>
+    <text x="50%" y="55%" dominant-baseline="middle" text-anchor="middle" font-family="system-ui, -apple-system, sans-serif" font-size="75" font-weight="900" fill="currentColor" style="color: #14b8a6;">
         Deanova <tspan style="color: #1e293b;" fill="currentColor">ERP</tspan>
     </text>
 </svg>
